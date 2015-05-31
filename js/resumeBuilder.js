1f06cba4db05292bc620774fe9aa6fd34fd6c031
Pragadeesh C <cpragadeesh@gmail.com>
2015-06-01 02:16:49 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,8 @@
-$("#main").append("Pragadessh")
+var fname = "Pragadeesh"
+var frole = "Web"
+
+HTMLheaderName = HTMLheaderName.replace("%data%", fname);
+HTMLroleName = HTMLroleName.replace("%data%", frole);
+
+$("header").append(HTMLheaderName);
+$("header").prepend(HTMLroleName);
